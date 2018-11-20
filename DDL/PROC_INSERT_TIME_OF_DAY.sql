28220fe3cd05a2cae06d492ac94151f0143cd634
Athlestan <fgrobeln@gmail.com>
2018-11-20 19:41:16 +0100

    Do testów poprawności danych

--- DDL/PROC_INSERT_TIME_OF_DAY.sql
+++ DDL/PROC_INSERT_TIME_OF_DAY.sql
@@ -8,17 +8,17 @@ BEGIN
    	SET NOCOUNT ON;
  
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('22:00:00.000','05:59:59.000','night');
+   	VALUES ('22:00:00.000','06:00:00.000','night');
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('06:00:00.000','09:59:59.000','morning');
+   	VALUES ('06:00:00.000','10:00:00.000','morning');
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('10:00:00.000','11:59:59.000','forenoon');
+   	VALUES ('10:00:00.000','12:00:00.000','forenoon');
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('12:00:00.000','14:59:59.000','early afternoon');
+   	VALUES ('12:00:00.000','15:00:00.000','early afternoon');
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('15:00:00.000','17:59:59.000','late afternoon');
+   	VALUES ('15:00:00.000','18:00:00.000','late afternoon');
    	INSERT INTO dbo.D_TIME_OF_DAY (Time_from, Time_to,  Time_of_day)
-   	VALUES ('18:00:00.000','21:59:59.000','evening');
+   	VALUES ('18:00:00.000','22:00:00.000','evening');
  
  
 END
